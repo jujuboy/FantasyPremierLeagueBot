592eadc556736d68fe7e6267b5194b0799aba6e7
Juan Rajagopal <jr422@ic.ac.uk>
2024-07-01 20:34:04 +0100

    Within the elements module, added a basic FPL Team class.

--- src/elements.py
+++ src/elements.py
@@ -63,8 +63,44 @@ class Player():
         if not isinstance(new_team, str):
             raise TypeError("New team argument must be a string.")
         self.__team = str(new_team)
-    
+
     def price_change(self, value:float):
+        """
+        Method to implement price change of a player.
+        """
         if not (isinstance(value, float) or isinstance(value, int)):
             raise TypeError("Value argument must be a float or int.")
         self.__price += value
+
+class Fpl_Team():
+    """
+    Class for FPL team.
+    """
+    def __init__(self, player_prices:list,
+                 player_names:list, player_positions:list,
+                 player_teams:list):
+        self.__players = [Player(price=player_prices[i],
+                                 name=player_names[i],
+                                 team=player_teams[i],
+                                 pos=player_positions[i]) for i in range(len(player_names))]
+    
+    def players(self):
+        """
+        Method to return player names.
+        """
+        return self.__players
+
+def main():
+
+    player_names = ['James', 'John']
+    player_teams = ['LIV', 'MNC']
+    player_positions = ['GKP', 'DEF']
+    player_prices = [8.0, 5.0]
+
+    my_team = Fpl_Team(player_positions=player_positions,
+                    player_names=player_names,
+                    player_teams=player_teams,
+                    player_prices=player_prices)
+
+if __name__ == main():
+    main()
